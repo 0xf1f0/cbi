4048cf2b7c005082392a7236728fca57156e2e54
0xf1f0 <oobey1@aol.com>
2017-10-22 15:29:24 -0400

    Add stations to map

--- ccbaynav/views.py
+++ ccbaynav/views.py
@@ -3,4 +3,19 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.db import models
+
 # Create your views here.
+'''
+This class will let admin users add stations to the map
+Class Instance Variables:
+    :param sName = Name of the station 
+    :param sLatitude: Latitude of the station
+    :param sLongitude: Longitude of the station
+'''
+
+
+class Map(models.Model):
+    mName = models.CharField(max_length=255)
+    mLatitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    mLongitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
