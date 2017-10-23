23ba31dd1919fea64a5927b53823eaafcce2491f
0xf1f0 <oobey1@aol.com>
2017-10-23 02:07:09 -0400

    Add google maps to template

--- cbi/settings.py
+++ cbi/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -35,7 +34,6 @@ SECRET_KEY = 'z7@)zqiqw0*f$=@4jl)53qs!6s4k!hwvxh@y_mdmbim)zy@@o#'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -80,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cbi.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -90,7 +87,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -110,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -124,11 +119,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
 
 # Get the Google maps API key from file in the current directory
 google_maps_api_key_path = os.path.relpath('cbi/google_maps_api_key.txt')
--- ccbaynav/static/js/script.js
+++ ccbaynav/static/js/script.js
@@ -1,20 +1,16 @@
 /**
  * Initialize and display Google maps
- * Add Google marker to the data stations.
- * The Stations are Bob Hall, Lexington, and Port Aransas
  */
-
 
 var initial_lat = 27.81688;         //  The corresponding latitude of map center at initialization.
 var initial_long = -97.344177;      //  The corresponding longitude of map center at initialization.
-var initial_zoom = 10;
+var initial_zoom = 10;              //  The corresponding zoom of map center at initialization.
+var map;
 
 function initMap() {
-
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: initial_zoom,
         center: new google.maps.LatLng(initial_lat, initial_long),
         mapTypeId: 'roadmap'
     });
 }
-
