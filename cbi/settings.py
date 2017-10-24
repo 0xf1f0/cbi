f62fd378ef1041722dafbb3310e6c40cd50ed67c
0xf1f0 <oobey1@aol.com>
2017-10-24 08:19:06 -0400

    Migrated static files - migration 1

--- cbi/settings.py
+++ cbi/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# All settings common to all environments
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
